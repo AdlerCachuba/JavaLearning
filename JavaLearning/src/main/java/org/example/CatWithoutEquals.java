77cff9584807288623e9cb9a5e74e3b03b5b7800
AdlerCachuba <adlercachuba@gmail.com>
2022-12-29 19:44:20 -0300

    Equals and == Class

---
+++ JavaLearning/src/main/java/org/example/CatWithoutEquals.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Objects;
+
+public class CatWithoutEquals {
+
+    String nome;
+    Integer idade;
+
+    public CatWithoutEquals(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+
+}
