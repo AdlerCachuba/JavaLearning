78d37db4dfccd42c369beb28320bd31046b939e2
AdlerCachuba <adlercachuba@gmail.com>
2023-01-05 00:10:50 -0300

    CheckUnCheked ExceptionClass

---
+++ JavaLearning/src/main/java/org/example/exceptions/CheckedVsUncheckedClass.java
@@ -0,0 +1,16 @@
+package org.example.exceptions;
+
+
+
+public class CheckedVsUncheckedClass {
+    public static void main(String[] args) {
+
+        //Uma Checked Exception é verificada no tempo de compilação.
+        //Como por exemplo:
+        //public String nome3 = 3;
+        //É um erro que não deixa nem o java compilar.
+
+
+    }
+
+}
