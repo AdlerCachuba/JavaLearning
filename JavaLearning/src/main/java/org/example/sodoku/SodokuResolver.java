3b6d2e81ebcf35c409f96ad24b77b9ce878b0071
AdlerCachuba <adlercachuba@gmail.com>
2023-03-13 16:59:29 -0300

    Sodoku Resolver!

---
+++ JavaLearning/src/main/java/org/example/sodoku/SodokuResolver.java
@@ -0,0 +1,108 @@
+package org.example.sodoku;
+
+public class SodokuResolver {
+
+    private static final int GRID_SIZE = 9;
+
+    public static void main(String[] args) {
+
+        int[][] board = {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3}
+        };
+
+        if (solveBoard(board)) {
+            System.out.println("Solved!");
+        } else {
+            System.out.println("Unsolvable Board");
+        }
+        printBoard(board);
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("-----------");
+            }
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (column % 3 == 0 && column != 0) {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+        //Dessa maneira a gente encontra a posição to topo da esquerda do quadrado.
+        //Começa pelo 0, e vai até o 8 a contagem das linhas/colunas aqui
+        //Imagine que a gente quer saber um valor do primeiro bloco, linha 1 coluna 4
+        // localBoxRow = 1 / localBoxColumn = 2
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !isNumberInRow(board, number, row) &&
+                !isNumberInColumn(board, number, column) &&
+                !isNumberInBox(board, number, row, column);
+    }
+
+    private static boolean solveBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (board[row][column] == 0) {
+                    for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+                        if (isValidPlacement(board, numberToTry, row, column)) {
+                            board[row][column] = numberToTry;
+                            //Recursividade
+                            if (solveBoard(board)) {
+                                return true;
+                            }
+                            //Se não tiver um jeito de resolver colocamos ele pra 0 e depois vamos olhar novamente.
+                            else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
